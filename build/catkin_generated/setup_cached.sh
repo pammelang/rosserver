afc8696cde12a18d6851c4f373a27b6830ad9a17
Pammela <poppyfights@gmail.com>
2018-04-17 01:19:24 +0800

    Updated bash script

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/pamm/rosserver/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/pamm/rosserver/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/pamm/rosserver/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/pamm/rosserver/src:$ROS_PACKAGE_PATH"
