017b78e86309f45d33dd8b200f60719c9ca2c087
Pammela Ng <poppyfights@gmail.com>
2018-04-17 09:04:31 +0000

    Bash script for automatic streaming working!!!

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
-(
+
+while true; do 
+    if [[ "$(netstat -a | grep 9090)" ]]; then   
+        echo "rosbridge found!!!!!!!!" 
+        node src/index.js 
+        break
+    else
+        echo "rosbridge not found" 
+        sleep 1
+    fi
+done &
+
 roscore &
 chmod 777 /dev/video1 && roscd gscam &&  export GSCAM_CONFIG="v4l2src device=/dev/video1 ! video/x-raw-rgb,framerate=30/1 ! ffmpegcolorspace" && rosrun gscam gscam &
-roslaunch rosbridge_server rosbridge_websocket.launch 
-echo ros processes running 
-) &&  
-node src/index.js
+roslaunch rosbridge_server rosbridge_websocket.launch &
