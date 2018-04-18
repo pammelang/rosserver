c768cd0ac276c3bcfadad35fb1f767a5ed5aba87
Pammela Ng <poppyfights@gmail.com>
2018-04-19 02:48:59 +0800

    Working control + streaming ros rpi

--- new/index.js
+++ new/index.js
@@ -4,9 +4,10 @@ var rpio = require('rpio');
 
 //// RPI GPIO SET UP 
 var pin1 = 12;           /* P12/GPIO18 */
-var pin2 = 32;           /* P32/GPIO12 */
+var pin2 = 33;           /* P33/GPIO33 */
 var range = 2000;     
-var max = 200;          /*   the bottom 8th of a larger scale */
+var max = 800;        
+var value = 100;
 var clockdiv = 128;       /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
 rpio.init({gpiomem: false});
 rpio.open(pin1, rpio.PWM);
@@ -18,6 +19,7 @@ rpio.pwmSetRange(pin2, range);
 //// ROS SET UP
 var ros = new ROSLIB.Ros(); 
 ros.connect('ws://localhost:9090');
+
 var listener = new ROSLIB.Topic({
     ros : ros,
     name : '/camera/image_raw/compressed',
@@ -25,7 +27,7 @@ var listener = new ROSLIB.Topic({
 });
 
 //// WEBSOCKET SET UP 
-const socket = io.connect("http://10.12.87.46:3000/video", { secure: true, reconnect: true });
+const socket = io.connect("http://10.12.253.106:3000/video", { secure: true, reconnect: true });
 socket.on('connect', function(){
     console.log('connected to server');
     socket.emit('connection', namespace='/video', {data: 'ROS client is connected!'});
@@ -38,19 +40,32 @@ socket.on('connect', function(){
 });
 socket.on('throttleup', function() {
     console.log('moving foward');
-    rpio.pwmSetData(pin1, max);
-    rpio.pwmSetData(pin2, max); 
+    rpio.pwmSetData(pin1, value);
+    rpio.pwmSetData(pin2, value); 
 });
 
 socket.on('throttleleft', function(){
     console.log('moving left');
-    rpio.pwmSetData(pin1, max);
+    rpio.pwmSetData(pin1, value);
     rpio.pwmSetData(pin2, 0);
 });
 socket.on('throttleright', function(){
     console.log('moving right');
     rpio.pwmSetData(pin1, 0);
-    rpio.pwmSetData(pin2, max);
+    rpio.pwmSetData(pin2, value);
+});
+socket.on('throttlestop', function(){
+    console.log('stopping');
+    rpio.pwmSetData(pin1, 0);
+    rpio.pwmSetData(pin2, 0);
+});
+socket.on('throttleslow', function(){
+    value = (value + 50 < max) ? value + 50 : max 
+    console.log('slowing down' + value);
+});
+socket.on('throttlefast', function(){
+    value = (value - 50 > 0) ? value - 50 : 0 
+    console.log('speeding up' + value);
 });
 socket.on('disconnect', function() {
     rpio.open(pin1, rpio.INPUT);
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
-source /rosserver/devel/setup.bash
 
 chmod 777 /dev/video0 && roscd gscam &&  export GSCAM_CONFIG="v4l2src device=/dev/video0 ! video/x-raw-rgb,framerate=30/1 ! ffmpegcolorspace"
 
@@ -9,14 +8,13 @@ rosrun gscam gscam &
 roslaunch rosbridge_server rosbridge_websocket.launch &
 
 while true; do 
+    sleep 5
     if [[ "$(netstat -a | grep 9090)" ]]; then   
         echo "rosbridge found!!!!!!!!" 
         node /home/pi/rosserver/new/index.js 
         break
     else
         echo "rosbridge not found" 
-        sleep 1
     fi
 done &
-
 
